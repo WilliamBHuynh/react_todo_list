11c53a9c166702e1990759563b2fbeb7d38b10f6
WilliamBHuynh <williamhuynh2@gmail.com>
2020-12-27 11:42:19 -0500

    Updates

---
+++ precache-manifest.b99d89a881ccaacb19ee1c34683b02c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70a3d73d32284ca6f5366c5602c1824",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "207d82797a1ab9ae90e9",
+    "url": "/react_todo_list/static/css/main.b9c2e552.chunk.css"
+  },
+  {
+    "revision": "778b1967b81bcb6a00bf",
+    "url": "/react_todo_list/static/js/2.a77f6130.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_list/static/js/2.a77f6130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207d82797a1ab9ae90e9",
+    "url": "/react_todo_list/static/js/main.fa39c599.chunk.js"
+  },
+  {
+    "revision": "ec735bf35178795da299",
+    "url": "/react_todo_list/static/js/runtime-main.f4d1e802.js"
+  }
+]);
